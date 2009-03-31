84af3f1a79519607d683854729155284c3cddfef
wokamoto <wokamoto@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-31 08:21:09 +0000

    [Twicon for WordPress] Update ver.1.2.6
    git-svn-id: https://plugins.svn.wordpress.org/twicon-for-wordpress/trunk@106537 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- twicon.php
+++ twicon.php
@@ -4,7 +4,7 @@ Plugin Name: Twicon for WordPress
 Plugin URI: http://wppluginsj.sourceforge.jp/twicon/
 Description: Let's show the Twitter avatar (Twicon) to your user with those comments of you in the Web site.
 Author: wokamoto
-Version: 1.2.5
+Version: 1.2.6
 Author URI: http://dogmap.jp/
 
 License:
@@ -52,9 +52,10 @@ add_filter('profile_image_url/twicon.php', 'set_profile_image_url', 10, 2);
 /******************************************************************************
  * define
  *****************************************************************************/
-define('TWICON_EXPIRED', 12);		// Request cache expired (hours)
-define('TWICON_CACHE', true);		// Icon File Cache (true or false)
-define('TWICON_LINK_TWITTER', false);	// Twicon link Twitter (true or false)
+define('TWICON_EXPIRED', 12);			// Request cache expired (hours)
+define('TWICON_CACHE', true);			// Icon File Cache (true or false)
+define('TWICON_CACHE_DIR', 'cache/twicon');	// Icon File Cache Directory
+define('TWICON_LINK_TWITTER', false);		// Twicon link Twitter (true or false)
 
 
 /******************************************************************************
@@ -70,11 +71,18 @@ if( !function_exists('get_option') ) {
  * twiconController Class
  *****************************************************************************/
 class twiconController {
+	var $cache = '';
+
 	var $_avatars = array();
 	var $_meta_value = array();
 	var $_cache_path = '';
 	var $_cache_url = '';
 	var $_option_update = false;
+	var $_suffix = array(
+		 'mini'   => '_mini'
+		,'normal' => '_normal'
+		,'big'    => '_bigger'
+		);
 
 	/**********************************************************
 	* Constructor
@@ -90,40 +98,41 @@ class twiconController {
 		$this->_avatars = get_option('twicon');
 
 		if (defined('TWICON_CACHE') && TWICON_CACHE) {
-			$this->_cache_path = $this->contentDir('/cache/twicon/');
-			$this->_cache_url  = $this->contentUrl('/cache/twicon/');
-			if( !file_exists($this->contentDir('/cache/')) )
-				@mkdir($this->contentDir('/cache/'), 0777);
+			$this->_cache_path = $this->contentDir( TWICON_CACHE_DIR );
+			$this->_cache_url  = $this->contentUrl( TWICON_CACHE_DIR );
+			$mode = 0777;
+			if( !file_exists( dirname($this->_cache_path) ) )
+				@mkdir( dirname($this->_cache_path), $mode );
 			if( !file_exists($this->_cache_path) )
-				@mkdir($this->_cache_path, 0777);
-			$this->_cache = file_exists($this->_cache_path);
+				@mkdir( $this->_cache_path, $mode );
+			$this->cache = file_exists($this->_cache_path);
 		}
 	}
 
 	// contentDir
 	function contentDir($path = '') {
-		return (!defined('WP_CONTENT_DIR')
+		return trailingslashit(trailingslashit(!defined('WP_CONTENT_DIR')
 			? WP_CONTENT_DIR
-			: ABSPATH . 'wp-content'
-			) . $path;
+			: trailingslashit(ABSPATH) . 'wp-content'
+			) . $path);
 	}
 
 	// contentUrl
 	function contentUrl($path = '') {
-		return (!defined('WP_CONTENT_URL')
+		return trailingslashit(trailingslashit(!defined('WP_CONTENT_URL')
 			? WP_CONTENT_URL
-			: get_option('siteurl') . '/wp-content'
-			) . $path;
+			: trailingslashit(get_option('siteurl')) . 'wp-content'
+			) . $path);
 	}
 
 	// pluginsDir
 	function pluginsDir($path = '') {
-		return $this->contentDir( '/plugins' . $path );
+		return trailingslashit($this->contentDir( 'plugins/' . $path ));
 	}
 
 	// pluginsUrl
 	function pluginsUrl($path = '') {
-		return $this->contentUrl( '/plugins' . $path );
+		return trailingslashit($this->contentUrl( 'plugins/' . $path ));
 	}
 
 	// Function updateAvatars
@@ -145,17 +154,14 @@ class twiconController {
 		if( isset($result['profile_image_url']) && $result['profile_image_url'] !== false && !empty($result['profile_image_url']) ) {
 			$safe_alt = ( false === $alt ? '': attribute_escape( $alt ));
 
-			$img_url     = $this->_unhtmlentities($result['profile_image_url']);
+			$img_url     = $result['profile_image_url'];
 			$expired     = $result['expiration_date'];
 			$name        = (isset($result['name']) ? $result['name'] : '');
 			$twitter_url = (isset($result['twitter_url']) ? $result['twitter_url'] : '');
 			$avatar      = str_replace("'", '"', $avatar);
 
 			if ( strpos($img_url, 'static.twitter.com') === false ) {
-				$img_url_en  = trim($this->_utf8_uri_encode($img_url));
-				if ( !empty($img_url_en) && $img_url != $img_url_en ) {
-					$img_url = str_replace('_bigger', '', $img_url_en);
-				} elseif ( defined('TWICON_CACHE') && TWICON_CACHE ) {
+				if ( $this->cache ) {
 					$cache_file_name = $this->_cache_file_name($img_url, $size);
 					if ( $this->_cache_file_exists($this->_cache_path . $cache_file_name, $expired) )
 						$img_url = $this->_cache_url . $cache_file_name;
@@ -340,7 +346,7 @@ class twiconController {
 		$twitter_url       = '';
 
 		if ( isset($this->_avatars[$id]) && isset($this->_avatars[$id]['expiration_date']) && time() < $this->_avatars[$id]['expiration_date'] ) {
-			$profile_image_url = (isset($this->_avatars[$id]['profile_image_url']) ? $this->_avatars[$id]['profile_image_url'] : '');
+			$profile_image_url = $this->_unhtmlentities(isset($this->_avatars[$id]['profile_image_url']) ? $this->_avatars[$id]['profile_image_url'] : '');
 			$expiration_date   = $this->_avatars[$id]['expiration_date'];
 			$name              = (isset($this->_avatars[$id]['name']) ? $this->_avatars[$id]['name'] : '');
 			$twitter_url       = (isset($this->_avatars[$id]['twitter_url']) ? $this->_avatars[$id]['twitter_url'] : '');
@@ -382,22 +388,28 @@ class twiconController {
 		if ( strpos($profile_image_url, 'static.twitter.com') === false ) {
 			if ( preg_match('/^(https?:\/\/.*)(_[^\/\._]*)(\.(jpe?g|gif|png))$/i', $profile_image_url, $matches) ) {
 				$size = (int) ( is_numeric($size) ? $size : '96' );
-				if ( defined('TWICON_CACHE') && TWICON_CACHE ) {
-					$suffix = '_bigger';
+				if ( $this->cache ) {
+					$suffix = $this->_suffix['big'];
 				} else {
 					if ($size <= 24)
-						$suffix = '_mini';
+						$suffix = $this->_suffix['mini'];
 					elseif ($size <= 48)
-						$suffix = '_normal';
+						$suffix = $this->_suffix['normal'];
 					else
-						$suffix = '_bigger';
+						$suffix = $this->_suffix['big'];
 				}
 				$profile_image_url = $matches[1] . $suffix . $matches[3];
 			}
 			unset($matches);
 		}
 
+		// Icon Image URL percent encoding
+		$image_url_encode  = trim($this->_utf8_uri_encode($profile_image_url));
+		if ( !empty($image_url_encode) && $profile_image_url != $image_url_encode )
+			$profile_image_url = str_replace(array($this->_suffix['mini'], $this->_suffix['normal'], $this->_suffix['big']), '', $image_url_encode);
+
 		$profile_image_url = apply_filters('profile_image_url/twicon.php', $profile_image_url, $id);
+
 		$result = compact('profile_image_url', 'expiration_date', 'name', 'twitter_url');
 		$this->_avatars[$id] = $result;
 
@@ -439,6 +451,7 @@ class twiconController {
 		return $img_resized;
 	}
 
+	// Function _unhtmlentities
 	function _unhtmlentities($string){
 		$encoding = strtolower(mb_detect_encoding($string));
 		if (!preg_match("/^utf/", $encoding) and $encoding != 'ascii')
@@ -508,7 +521,7 @@ if ( strpos($_SERVER['PHP_SELF'], basename(__FILE__)) === false ) {
 	add_filter('get_avatar', array(&$twicon, 'getAvatar'), 10, 5);
 	add_action('shutdown',   array(&$twicon, 'updateAvatars'));
 
-} elseif ( defined('TWICON_CACHE') && TWICON_CACHE && isset($_GET['url']) ) {
+} elseif ( $twicon->cache && isset($_GET['url']) ) {
 	// Get Image from Cache
 	$img_url  = base64_decode($_GET['url']);
 	$img_size = (int) (isset($_GET['size']) ? stripslashes($_GET['size']) : 48);
